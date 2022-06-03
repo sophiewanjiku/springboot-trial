9b43e648af2a889f85174aeac3a0d7f9a39cbc56
sophie <skwanjiku@kabarak.ac.ke>
2022-06-03 12:19:03 +0300

    initial spring setup

---
+++ src/main/java/com/project1/mavenproject/MavenprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project1.mavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project1/mavenproject/MavenprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.mavenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
